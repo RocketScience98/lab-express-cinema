80e60e547c4929fabe82c97c846c181d68ece32f
RocketScience98 <luisalbertodavila@gmail.com>
2021-05-10 16:13:47 -0500

    Donerino

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
+const Movie = require("../models/Movie.model")
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+router.get('/movies', (req, res, next) => {
+Movie.find()
+.then(moviesDB=>{
+    console.log("Movies shown",moviesDB);
+res.render('movies',{movies: moviesDB});
+}).catch(e=> console.log("DB error",e))
+})
+
+router.get("/movie/:id", (req, res, next) => {
+    const { id } = req.params;
+    Movie.findById(id)
+      .then((moviebyId) => {
+        console.log("Movie Chosen", moviebyId);
+        res.render("movie-details", { movie: moviebyId });
+      })
+      .catch((error) => {
+        console.log("Error while showing movie details", error);
+      });
+  });
 
 module.exports = router;
